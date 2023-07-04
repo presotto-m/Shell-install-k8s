7684108c9c187f13ba095601a0f417a38c155845
Matheus Presotto <83012707+presotto-m@users.noreply.github.com>
2023-07-04 11:23:40 -0700

    Create install-k8s.sh

---
+++ install-k8s.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Instalação dos módulos do kernel
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Configurando parametros sysctl
+
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sudo sysctl --system
+
+# Instalando e configurando pacotes K8S
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# Instalando e configurando GO
+sudo apt update
+wget https://golang.org/dl/go1.20.1.linux-amd64.tar.g
+sudo tar -xf go1.20.1.linux-amd64.tar.gz -C /usr/local
+echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
+source .profile
+
+# Instalando e configurando cri-docker
+git clone https://github.com/Mirantis/cri-dockerd.git
+cd cri-dockerd
+make cri-dockerd
+mkdir -p /usr/local/bin
+install -o root -g root -m 0755 cri-dockerd /usr/local/bin/cri-dockerd
+install packaging/systemd/* /etc/systemd/system
+sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+systemctl daemon-reload
+systemctl enable cri-docker.service
+systemctl enable --now cri-docker.socket
+
+# Iniciando cluster
+kubeadm init
+
+# configurando kubectl
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Gerando o comando join e executando nos nodes
+kubeadm token create --print-join-command
