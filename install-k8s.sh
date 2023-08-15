bdcfff55a156193f1de9371e3ff65d1c3d44ef85
Matheus Presotto <83012707+presotto-m@users.noreply.github.com>
2023-08-15 04:57:25 -0700

    Update install-k8s.sh

--- install-k8s.sh
+++ install-k8s.sh
@@ -19,8 +19,7 @@ sudo apt-get install gnupg
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 mkdir /etc/apt/keyrings
-wget -O- https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor >kubernetes-archive-keyring.gpg
-mv kubernetes-archive-keyring.gpg /etc/apt/trusted.gpg.d/
+curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
